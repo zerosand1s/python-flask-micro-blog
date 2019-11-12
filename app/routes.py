5a0cc4dd0157fae6d150b9e4dc4b7812602936e8
harshal <harshal.g@infostretch.com>
2019-11-12 15:56:20 +0530

    add templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import flask_app
+
 
 @flask_app.route('/')
 @flask_app.route('/index')
 def index():
-    return 'First Flask App...'
+    user = {'username': 'Harshal'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
