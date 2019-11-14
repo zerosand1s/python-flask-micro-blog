8cd40f72e728e2397b02758afdd0af6e9a25e4c8
harshal <harshal.g@infostretch.com>
2019-11-14 15:08:05 +0530

    add sign up form. set sign up, login and logout flows

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 flask_app = Flask(__name__)
 flask_app.config.from_object(Configuration)
 db = SQLAlchemy(flask_app)
 migrate = Migrate(flask_app, db)
+login = LoginManager(flask_app)
+login.login_view = 'login'
 
 from app import routes, models
--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,26 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+
+from app.models import User
+
+
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = StringField('Password', validators=[DataRequired()])
+    repeat_password = StringField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username already exists. Please choose different username')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email already exists. Please use different email address')
 
 
 class LoginForm(FlaskForm):
